92a44479969cf6fe5b6807409f28713ee6825a5c
JakubTeichman <jteichman@student.agh.edu.pl>
2025-03-30 16:03:16 +0200

    Remove unnesesery files

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, jsonify
-from app.models import Photo
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
+    from app.models import Photo
     return jsonify({"message": "Foto-Forensic API działa!"})
 

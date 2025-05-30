b0501389c622716b7f2639cc7bf7d7658a9dc6bf
JakubTeichman <jteichman@student.agh.edu.pl>
2025-05-30 17:48:33 +0200

    Demo created
    Simple backend - frontend connection check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Compare from "./components/Compare";
 
 const App = () => {
   return (
     <div>
-
+      <h1>Porównanie PRNU</h1>
+      <Compare />
+      <footer>
+        <p>© 2023 Porównanie PRNU. Wszelkie prawa zastrzeżone.</p>
+      </footer>
     </div>
   );
 };
